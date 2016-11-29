ac612bc9991736bbea3e0768a2ce7cb81aaacb95
cchristo <cchristo@venasolutions.com>
2016-11-29 14:31:33 -0500

    add BatchImageResizer (easy drag/drop that I made for my mom)

---
+++ BatchImageResizer/BatchImageResizer/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace BatchImageResizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ColName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColPath = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColWidth = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColHeight = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColNewWidth = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColNewHeight = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColName,
+            this.ColPath,
+            this.ColWidth,
+            this.ColHeight,
+            this.ColNewWidth,
+            this.ColNewHeight});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 26);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(807, 449);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // ColName
+            // 
+            this.ColName.HeaderText = "Name";
+            this.ColName.Name = "ColName";
+            this.ColName.ReadOnly = true;
+            // 
+            // ColPath
+            // 
+            this.ColPath.HeaderText = "Path";
+            this.ColPath.Name = "ColPath";
+            this.ColPath.ReadOnly = true;
+            // 
+            // ColWidth
+            // 
+            this.ColWidth.HeaderText = "Width";
+            this.ColWidth.Name = "ColWidth";
+            this.ColWidth.ReadOnly = true;
+            // 
+            // ColHeight
+            // 
+            this.ColHeight.HeaderText = "Height";
+            this.ColHeight.Name = "ColHeight";
+            this.ColHeight.ReadOnly = true;
+            // 
+            // ColNewWidth
+            // 
+            this.ColNewWidth.HeaderText = "NewWidth";
+            this.ColNewWidth.Name = "ColNewWidth";
+            this.ColNewWidth.ReadOnly = true;
+            // 
+            // ColNewHeight
+            // 
+            this.ColNewHeight.HeaderText = "NewHeight";
+            this.ColNewHeight.Name = "ColNewHeight";
+            this.ColNewHeight.ReadOnly = true;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.label1);
+            this.flowLayoutPanel1.Controls.Add(this.textBox1);
+            this.flowLayoutPanel1.Controls.Add(this.label2);
+            this.flowLayoutPanel1.Controls.Add(this.textBox2);
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(807, 26);
+            this.flowLayoutPanel1.TabIndex = 1;
+            this.flowLayoutPanel1.WrapContents = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 6);
+            this.label1.Margin = new System.Windows.Forms.Padding(3, 6, 3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "New Size (%)";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(78, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(403, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Resize && Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(184, 6);
+            this.label2.Margin = new System.Windows.Forms.Padding(3, 6, 3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "JPG Save Quality (%)";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(297, 3);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "80";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(807, 475);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Batch image to jpeg resizer - drag and drop your images from windows explorer int" +
+    "o the below table";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColPath;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColWidth;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColHeight;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColNewWidth;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColNewHeight;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ BatchImageResizer/BatchImageResizer/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BatchImageResizer
+{
+    public partial class Form1 : Form
+    {
+        private FileDragDropHandler ddh;
+        public Form1()
+        {
+            InitializeComponent();
+            ddh=new FileDragDropHandler(dataGridView1);
+            ddh.FilesDropped += new DragDropOccured(ddh_FilesDropped);
+            textBox1.Text = "50";
+        }
+
+        void ddh_FilesDropped(string[] files)
+        {
+            dataGridView1.Rows.Clear();
+            foreach (var file in files)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                using (FileStream stream = new FileStream(file, FileMode.Open, FileAccess.Read))
+                {
+                    Image bitmap = Image.FromStream(stream,true,false);
+                    dataGridView1.Rows.Add(fileInfo.Name, fileInfo.Directory.FullName, bitmap.Width, bitmap.Height, bitmap.Width, bitmap.Height);
+                }
+            }
+            textBox1_TextChanged(null, null);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            double factor;
+            if (!double.TryParse(textBox1.Text,out factor)) factor = 100;
+            factor = factor/100;
+            if (factor <= 0) factor = 1;
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                row.Cells["ColNewWidth"].Value = (int)((int)row.Cells["ColWidth"].Value * factor);
+                row.Cells["ColNewHeight"].Value = (int)((int)row.Cells["ColHeight"].Value * factor);
+            }
+        }
+        private ImageCodecInfo getEncoderInfo(string mimeType)
+        {
+            // Get image codecs for all image formats
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageEncoders();
+
+            // Find the correct image codec
+            for (int i = 0; i < codecs.Length; i++)
+                if (codecs[i].MimeType == mimeType)
+                    return codecs[i];
+            return null;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ImageCodecInfo jpegCodec = getEncoderInfo("image/jpeg");
+            if (jpegCodec == null)
+            {
+                MessageBox.Show("jpeg codec not found. cannot proceed");
+                return;
+            }
+            button1.Enabled = false;
+            textBox1.Enabled = false;
+            int quality;
+            if (!int.TryParse(textBox2.Text, out quality) || quality <= 0)
+            {
+                quality = 100;
+                textBox2.Text = quality.ToString();
+            }
+            EncoderParameter qualityParam = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, quality);
+            EncoderParameters encoderParams = new EncoderParameters(1);
+            encoderParams.Param[0] = qualityParam;
+            
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                FileInfo fileInfo = new FileInfo(Path.Combine((string)row.Cells["ColPath"].Value,(string)row.Cells["ColName"].Value));
+                using (FileStream stream = new FileStream(fileInfo.FullName, FileMode.Open, FileAccess.Read))
+                {
+                    Image bitmap = Image.FromStream(stream, true, false);
+                    Bitmap b = new Bitmap((int)row.Cells["ColNewWidth"].Value, (int)row.Cells["ColNewHeight"].Value);
+                    Graphics g = Graphics.FromImage(b);
+                    g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                    g.DrawImage(bitmap, 0, 0, b.Width, b.Height);
+                    b.Save(Path.Combine(fileInfo.DirectoryName, Path.GetFileNameWithoutExtension(fileInfo.FullName) + "_resized" + ".jpg"),
+                        jpegCodec, encoderParams);
+                }
+                Text = "processing " + fileInfo.FullName;
+                Application.DoEvents();
+            }
+            Text = "PROCESSING COMPLETE";
+            button1.Enabled = true;
+            textBox1.Enabled = true;
+        }
+    }
+}
