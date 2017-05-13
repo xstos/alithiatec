b22f16c460416ee980caa9830afe893f1fc9c21b
chris <cchristo@venasolutions.com>
2017-05-13 10:52:49 -0400

    add DaveScraper to fetch weekly stock ticker numbers

---
+++ DaveScraper/DaveScraper/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace DaveScraper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startDatePicker = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.endDatePicker = new System.Windows.Forms.DateTimePicker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.savePathTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // startDatePicker
+            // 
+            this.startDatePicker.Location = new System.Drawing.Point(47, 12);
+            this.startDatePicker.Name = "startDatePicker";
+            this.startDatePicker.Size = new System.Drawing.Size(200, 20);
+            this.startDatePicker.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Start";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "End";
+            // 
+            // endDatePicker
+            // 
+            this.endDatePicker.Location = new System.Drawing.Point(47, 38);
+            this.endDatePicker.Name = "endDatePicker";
+            this.endDatePicker.Size = new System.Drawing.Size(200, 20);
+            this.endDatePicker.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox1.Location = new System.Drawing.Point(12, 90);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(100, 393);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "comp\r\nnya";
+            // 
+            // savePathTextBox
+            // 
+            this.savePathTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.savePathTextBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.savePathTextBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.FileSystem;
+            this.savePathTextBox.Location = new System.Drawing.Point(46, 64);
+            this.savePathTextBox.Name = "savePathTextBox";
+            this.savePathTextBox.Size = new System.Drawing.Size(847, 20);
+            this.savePathTextBox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Path";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(253, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 42);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(899, 62);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(33, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox3.Location = new System.Drawing.Point(118, 90);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox3.Size = new System.Drawing.Size(814, 393);
+            this.textBox3.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(944, 495);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.savePathTextBox);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.endDatePicker);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.startDatePicker);
+            this.Name = "Form1";
+            this.Text = "DaveScraper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker startDatePicker;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker endDatePicker;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox savePathTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ DaveScraper/DaveScraper/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DaveScraper
+{
+    public partial class Form1 : Form
+    {
+        WebBrowser browser = new WebBrowser();
+        List<OHLC> results = new List<OHLC>();
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load;
+            
+        }
+
+        IEnumerable<OHLC> GetDateSequence()
+        {
+            foreach (var symbol in textBox1.Lines)
+            {
+                foreach (var date in GetWorkingDaysBetween(startDatePicker.Value, endDatePicker.Value))
+                {
+                    yield return new OHLC() { symbol = symbol, date = date };
+                }
+            }
+            yield break;
+        }
+        private void button1_Click(object sender1, EventArgs ee)
+        {
+            results.Clear();
+            textBox3.Clear();
+            browser.AllowNavigation = true;
+            var dateEnumerator = GetDateSequence().GetEnumerator();
+            if (!dateEnumerator.MoveNext()) return;
+            Action navigate = () =>
+            {
+                var url = createUrl(dateEnumerator.Current.symbol, dateEnumerator.Current.date);
+                
+                browser.Navigate(url);
+                
+            };
+            browser.DocumentCompleted += (sender, e) =>
+            {
+                if (e.Url.AbsolutePath != (sender as WebBrowser).Url.AbsolutePath)
+                {
+                    return; //see http://stackoverflow.com/questions/2777878/detect-webbrowser-complete-page-loading
+                }
+                var data = GetTableDataFromDocument(browser.Document, dateEnumerator.Current);
+                results.Add(data);
+                textBox3.AppendText(data.ToTabDelimitedString()+Environment.NewLine);
+                if (dateEnumerator.MoveNext())
+                {
+                    navigate();
+                }
+                else
+                {
+                    saveResults();
+                }
+            };
+            navigate();
+        }
+
+        void saveResults()
+        {
+            File.WriteAllLines(savePathTextBox.Text, results.Select(item => item.ToTabDelimitedString()));
+            Process.Start(savePathTextBox.Text);
+
+        }
+        static DateTime OffsetBackwardsTo(DateTime date, DayOfWeek day)
+        {
+            while (date.DayOfWeek > day) //sloppy but it's easier than doing the math
+            {
+                date = date.AddDays(-1);
+            }
+            return date;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            savePathTextBox.Text = Path.Combine(System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),"output.txt");
+            browser.ScriptErrorsSuppressed = true;
+            var lastSat = OffsetBackwardsTo(DateTime.Now, DayOfWeek.Saturday);
+            startDatePicker.Value = OffsetBackwardsTo(lastSat, DayOfWeek.Monday);
+            endDatePicker.Value = startDatePicker.Value.AddDays(4);
+        }
+
+        struct OHLC
+        {
+            public DateTime date { get; set; }
+            public string symbol { get; set; }
+            public string open { get; set; }
+            public string high { get; set; }
+            public string low { get; set; }
+            public string close { get; set; }
+            public string ToTabDelimitedString()
+            {
+                return string.Join("\t", new[] {date.ToShortDateString(), symbol, open, high, low, close});
+            }
+        }
+
+        static OHLC GetTableDataFromDocument(HtmlDocument document,OHLC fillme)
+        {
+            var tbl = document.GetElementById("historicalquote");
+            if (tbl==null) return fillme;
+            dynamic myTable = tbl.DomElement;
+            fillme.close = myTable.rows[2].cells[1].innerText;
+            fillme.open = myTable.rows[3].cells[1].innerText;
+            fillme.high = myTable.rows[4].cells[1].innerText;
+            fillme.low = myTable.rows[5].cells[1].innerText;
+            return fillme;
+        }
+        static IEnumerable<DateTime> GetWorkingDaysBetween(DateTime start, DateTime end)
+        {
+            while (start.Date <= end.Date)
+            {
+                if (start.DayOfWeek == DayOfWeek.Saturday || start.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    start = start.AddDays(1);
+                    continue;
+                }
+                yield return start.Date;
+                start = start.AddDays(1);
+            }
+        }
+
+        static string createUrl(string symbol, DateTime date)
+        {
+            var month = date.Month;
+            var day = date.Day;
+            var year = Microsoft.VisualBasic.Strings.Right(date.Year.ToString(),2);
+            return $"http://bigcharts.marketwatch.com/historical/default.asp?symb={symbol}&closeDate={month}%2F{day}%2F{year}";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog(this)==DialogResult.OK)
+            {
+                savePathTextBox.Text = saveFileDialog1.FileName;
+            }
+        }
+
+        
+    }
+}
