904800e7cca800763a1348783c1d523890122461
chris <cchristo@venasolutions.com>
2017-05-20 11:54:57 -0400

    add registry utils

---
+++ AlithiaLib/Extensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Win32;
+
+namespace AlithiaLib
+{
+    public static class Extensions
+    {
+        public static IEnumerable<RegistryValue> GetValues(this IEnumerable<RegistryKey> keys)
+        {
+            foreach (var key in keys)
+            {
+                var values = key.GetValueNames();
+                foreach (var valueName in values)
+                {
+                    yield return new RegistryValue(parent: key, kind: key.GetValueKind(valueName), value: key.GetValue(valueName), name: valueName);
+                }
+            }
+        }
+        public static IEnumerable<RegistryKey> GetAllRegistryKeys()
+        {
+            var hives = new[] { Registry.ClassesRoot, Registry.CurrentConfig, Registry.CurrentUser, Registry.LocalMachine, Registry.Users, Registry.PerformanceData };
+            return hives.SelectMany(hive => hive.ChildrenRecusive());
+        }
+
+        public static IEnumerable<RegistryKey> Children(this RegistryKey key)
+        {
+            return key.GetSubKeyNames().Where(s => s != "Wow6432Node").Select(key.OpenSubKey);
+        }
+        public static IEnumerable<RegistryKey> ChildrenRecusive(this RegistryKey key)
+        {
+            Stack<RegistryKey> keys = new Stack<RegistryKey>();
+            keys.Push(key);
+            RegistryKey current;
+            do
+            {
+                current = keys.Pop();
+                foreach (var child in current.Children())
+                {
+                    yield return child;
+                    keys.Push(child);
+                }
+            } while (keys.Count > 0);
+        }
+    }
+}
---
+++ AlithiaLib/RegistryValue.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using Microsoft.Win32;
+
+namespace AlithiaLib
+{
+    public class RegistryValue
+    {
+        private string name;
+
+        public static IEnumerable<DataColumn> GetColumns()
+        {
+            yield return new DataColumn("Parent", typeof(string));
+            yield return new DataColumn("Name", typeof(string));
+            yield return new DataColumn("Kind", typeof(RegistryValueKind));
+            yield return new DataColumn("Value", typeof(object));
+        }
+        public RegistryValueKind Kind { get; set; }
+        public RegistryKey Parent { get; set; }
+        public object Value { get; set; }
+        public string Name { get; set; }
+        public object[] values;
+        public RegistryValue(RegistryKey parent, RegistryValueKind kind, object value, string name)
+        {
+            this.Parent = parent;
+            this.Kind = kind;
+            this.Value = value;
+            Name = name;
+            values = new[] { parent.Name, name, kind, value };
+        }
+
+    }
+}
